fedad609b9909fc6cee5152d94e898d65d597252
21P31A04Q2 <99868755+21P31A04Q2@users.noreply.github.com>
2023-06-08 20:55:50 +0530

    m=int(input()) l=[] for i in range(1,m+1):     l.append(i) s = [str(integer) for integer in l] a_string = "".join(s) n = int(a_string) p=str(n) q=len(p) #(q) while n:     r=n%10**q     print(r)     n=n//10

---
+++ print_the_following_pattern.py
@@ -0,0 +1,15 @@
+m=int(input())
+l=[]
+for i in range(1,m+1):
+    l.append(i)
+s = [str(integer) for integer in l]
+a_string = "".join(s)
+n = int(a_string)
+p=str(n)
+q=len(p)
+#(q)
+while n:
+    r=n%10**q
+    print(r)
+    n=n//10
+    
